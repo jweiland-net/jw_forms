25dd73e0bd7471b94cb48fd6c809aa37e3e223ca
crontis <dev@crontis.net>
2020-08-12 10:31:38 +0200

    [FIX] Fixed invalid .sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -5,5 +5,5 @@ CREATE TABLE tx_jwforms_domain_model_form (
 	title varchar(255) DEFAULT '' NOT NULL,
 	file text,
 	url_to_file varchar(255) DEFAULT '' NOT NULL,
-	tags varchar(255) DEFAULT '' NOT NULL,
+	tags varchar(255) DEFAULT '' NOT NULL
 );
