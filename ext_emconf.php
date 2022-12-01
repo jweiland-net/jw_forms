fe0952a71bfe7a3b190bfedb121f8c090d0ac8ce
Stefan Froemken <froemken@gmail.com>
2022-12-01 10:29:11 +0100

    Add dependency for glossary2

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,6 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.32-11.5.99',
+            'glossary2' => '5.0.0-0.0.0',
         ],
         'conflicts' => [
         ],
