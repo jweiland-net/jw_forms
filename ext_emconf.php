b0f83575469827bcfe85cb35abef494c37a60277
Stefan Froemken <froemken@gmail.com>
2022-12-01 12:01:50 +0100

    Update version to 2.0.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,15 +1,14 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
     'title' => 'JW Forms',
-    'description' => 'This extension gives you the possibility to display title and file ' .
-        'of forms (PDF, ...) by starting letter and search forms of this list.',
+    'description' => 'With this extension you can provide a list of downloadable files in FE.',
     'category' => 'plugin',
     'author' => 'Stefan Froemken',
     'author_email' => 'sfroemken@jweiland.net',
     'author_company' => 'jweiland.net',
     'state' => 'stable',
     'clearCacheOnLoad' => 0,
-    'version' => '2.0.2',
+    'version' => '2.0.3',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.17-10.4.99',
