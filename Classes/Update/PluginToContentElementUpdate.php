1ebeeee0907114edd0e563bdd3edd4b2845d25f6
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-09-15 11:46:44 +0200

    [TASK] Added list_type to CType migration

---
+++ Classes/Update/PluginToContentElementUpdate.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package jweiland/jw-forms.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace JWeiland\JwForms\Update;
+
+use TYPO3\CMS\Install\Attribute\UpgradeWizard;
+use TYPO3\CMS\Install\Updates\AbstractListTypeToCTypeUpdate;
+
+/**
+ * With TYPO3 13 all plugins have to be declared as content elements (CType) insteadof "list_type"
+ */
+#[UpgradeWizard('jwforms_migratePluginsToContentElementsUpdate')]
+class PluginToContentElementUpdate extends AbstractListTypeToCTypeUpdate
+{
+    protected function getListTypeToCTypeMapping(): array
+    {
+        return [
+            'jwforms_forms' => 'jwforms_forms',
+        ];
+    }
+
+    public function getTitle(): string
+    {
+        return 'EXT:jw_forms - Migrate plugins to Content Elements';
+    }
+
+    public function getDescription(): string
+    {
+        return 'The modern way to register plugins for TYPO3 is to register them as content element types. ' .
+            'Running this wizard will migrate all jw_forms plugins to content element (CType)';
+    }
+}
