5b837a3e4bf710198ebafa5dd5140108dd154723
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-09-26 13:28:32 +0200

    [BUGFIX] Fixed CGL Issues

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -13,12 +13,12 @@ use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 ExtensionUtility::registerPlugin(
     'JwForms',
     'Forms',
-    'JW Forms'
+    'JW Forms',
 );
 
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['jwforms_forms'] = 'layout,select_key,pages,recursive';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['jwforms_forms'] = 'pi_flexform';
 ExtensionManagementUtility::addPiFlexFormValue(
     'jwforms_forms',
-    'FILE:EXT:jw_forms/Configuration/FlexForms/JwForms.xml'
+    'FILE:EXT:jw_forms/Configuration/FlexForms/JwForms.xml',
 );
